78b95dff510927b0ec63c99d5614cb51d58466a9
Bruce <brucelau.email@gmail.com>
2017-05-02 14:26:02 -0400

    savecunter to redis on each change

--- src/counter.js
+++ src/counter.js
@@ -20,19 +20,23 @@ init();
 exports.show = function(req, res){
 	res.json(counter);
 };
+
 exports.add = function(req, res){
 	//get req add number
 	var number = parseInt(req.params.number);
 	counter.counter += number;
+	saveCounter();
 	res.json(counter);
 };
 exports.sub = function(req, res){
 	//get req add number
 	var number = parseInt(req.params.number);
 	counter.counter -= number;
+	saveCounter();
 	res.json(counter);
 };
 exports.clear = function(req, res){
 	counter.counter = 0;
+	saveCounter();
 	res.json(counter);
 };
