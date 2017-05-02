3a79299e180cd5a9e0f00f57fdd63cdb7bf66283
Bruce <brucelau.email@gmail.com>
2017-05-02 18:50:12 -0400

    move index.js to app root directory

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
 require('dotenv').config();
-var counter = require('./counter');
+var counter = require('./src/counter');
 
 //routing definition
 app.get('/Counter/show', counter.show);
 app.put('/Counter/add/:number', counter.add);
 app.put('/Counter/sub/:number', counter.sub);
 app.put('/Counter/clear', counter.clear);
+app.use(express.static('./public'));
 
 app.listen(process.env.SVR_PORT, function () {
   console.log('gamp app listening on port', process.env.SVR_PORT, '!')
