51a19433e4dec0db1b6002cee3f0c0fb51445eb9
Bruce <brucelau.email@gmail.com>
2017-05-04 01:26:54 -0400

    add authentication module

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
+'use strict'
 var express = require('express');
 var app = express();
 require('dotenv').config();
 var counter = require('./src/counter');
 
+//authentication
+var path = require('path');
+var auth = require('express-authentication');
+var basic = require('express-authentication-basic');
+
+var login = basic(function(challenge, callback) {
+	if (challenge.username === 'bruce' && challenge.password === 'liu') {
+		callback(null, true, { user: 'charles' });
+	} else {
+		callback(null, false, { error: 'INVALID_PASSWORD' });
+	}
+});
+app.use(login);
+app.get('/', function(req, res,next) {
+	if (req.authenticated) {
+		next();
+	} else {
+		res.status(401).send();
+	}
+});
 //routing definition
 app.get('/Counter/show', counter.show);
 app.put('/Counter/add/:number', counter.add);
